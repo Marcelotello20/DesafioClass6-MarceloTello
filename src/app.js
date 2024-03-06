7ccb90fed1710dc4e7f0896dd7a19a85c2f95257
Marcelo Tello <zhelobeats@gmail.com>
2024-03-06 19:47:56 -0300

    agregando endpoint / y /products con el param limit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,32 @@
 const ProductManager = require('./components/ProductManager/ProductManager');
 const express = require('express')
 
+const app = express();
 const PM = new ProductManager(`${__dirname}/Productos.json`)
 
+//Endpoint principal
+app.get('/', (req, res) => {
+    res.send('¡Hola este es el desafio de la clase 6');
+});
 
-const app = async () => {
-    
+//Endpoint para buscar los productos
+app.get('/products', async (req, res) => {
+    try {
+        //Convertir a un numero el query param limit
+        const limit = parseInt(req.query.limit);
 
-}
+        //Obteniendo productos
+        const products = await PM.getProducts();
 
-app();
+        //Si limit es un valor valido devuelve los productos si no devuelve los productos
+        if (!isNaN(limit)) {
+            res.json(products.slice(0, limit))
+        } else {
+            res.json(products);
+        }
+    } catch(error) {
+        console.error('Error al buscar los productos', error);
+    }
+})
+
+app.listen(8080, () => console.log('Servidor Iniciado en el puerto 8080'));
